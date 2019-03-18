5f7c148247f5e7c9c68351477f82ba8b3ccdb64b
Spencer Meredith <Smeredith1119@gmail.com>
2019-03-18 16:51:18 -0700

    created delete

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const express = require("express");
 const server = express();
 const port = 7000;
 
+
 // Define the server next excute a function going to take
 //request handler call back 2 arg req and res req object
 // request object that come up to server and the response object sends a respone back
@@ -18,6 +19,71 @@ const port = 7000;
 server.get("/", (req, res) => {
     res.send("Hello World");
 });
+
+
+server.get('/api/users', (req, res) => {
+    db
+      .find()
+      .then(users => {
+        res.json(users);
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: 'Error Please check again' });
+      });
+  });
+
+
+  server.get('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+  
+
+    db.findById(id)
+      .then(user => {
+        if (user) {
+          res.json(user);
+        } else {
+          res
+            .status(404)
+            .json({ message: 'This user does not exist.' });
+        }
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: 'The user information cant be found.' });
+      });
+  });
+
+  server.post('/api/users', (req, res) => {
+    const user = req.body
+    db
+      .insert(user)
+      .then(users => {
+           res.status(201).json(users)
+      })
+     .catch(err => {
+        res.status(400).json({ message: "Please Give name information to user" })
+     })
+})
+
+server.delete('/api/users/:id', (req, res) => {
+    const id = req.params.id
+    db
+      .remove(id)
+      .then(users => {
+        if (users) {
+            res.json(users)
+        } else {
+            res.status(404).json({ message: "This user does not exist." })
+        }
+     })
+     .catch(err => {
+        res.status(500).json({ error: "Has been deleted Something wrong" })
+    })
+})
+
 
 // Last we need to setup a listen port and callback to run once excuted.
 
