0e098b375e4ec8dfa2f828be321241002b992195
Spencer Meredith <Smeredith1119@gmail.com>
2019-03-18 16:45:35 -0700

    added expres and port

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const express = require("express");
 // computer where server going to run syntax below will make an express app with the express moduel
 
 const server = express();
+const port = 7000;
 
 // Define the server next excute a function going to take
 //request handler call back 2 arg req and res req object
